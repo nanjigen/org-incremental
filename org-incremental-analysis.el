2914e736bf0048f6aa52b713e84efb92f894635e
nanigen <otoo.danieru@gmail.com>
2022-08-10 11:57:49 +0900

    Tangle licensing out to files

--- org-incremental-analysis.el
+++ org-incremental-analysis.el
@@ -1,1 +1,28 @@
+;;; org-incremental-analysis.el --- Incremental Writing System for org-mode -*- lexical-binding: t; -*-
+;;; Header:
 
+;; Author: Daniel Otto
+;; Version: 0.0
+;; Package-requires: ((emacs "26.3") (org "9.4"))
+
+;; URL: https://github.com/nanjigen/org-incremental
+
+;; Copyright (C) 2021-2022 Daniel Otto
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Incremental writing for org-mode
+;;
